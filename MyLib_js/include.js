94eea01491604fe1d9ca81ee6058967aa5fdd870
CWKSC <CWKSC.person@gmail.com>
2021-01-14 01:00:01 +0800

    Update

---
+++ MyLib_js/include.js
@@ -0,0 +1,11 @@
+var currentRoot = "";
+
+function load(src){
+	document.write(`<script src="${currentRoot}/${src}.js"></script>`);
+}
+
+
+currentRoot = "MyLib_js";
+load("HelloWorld");
+load("JoinFunction");
+load("XORTable");
