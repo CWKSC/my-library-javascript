521099a6922216b5c2aff12e89d29ee51632d5e0
CWKSC <CWKSC.person@gmail.com>
2021-02-07 19:23:54 +0800

    Update README.md and add printableAsciit to CTFTool

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 [cwksc.github.io/mylib_js/](https://cwksc.github.io/MyLib_js/)
 
-MyLib_js
+### MyLib_js
+
+https://cdn.jsdelivr.net/gh/CWKSC/MyLib_js/MyLib_js.js
 
 ```html
 <script type="text/javascript" 
@@ -8,7 +10,9 @@ MyLib_js
         async> </script>
 ```
 
-`CTFTool`
+### CTFTool
+
+https://cdn.jsdelivr.net/gh/CWKSC/MyLib_js/src/CTFTool.js
 
 ```html
 <script type="text/javascript" 
--- src/CTFTool.js
+++ src/CTFTool.js
@@ -1,5 +1,8 @@
 var CTFTool = {
 
+printableAsciiString: " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+printableAsciiArray: [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"],
+
 intToHexStringByEndian: function(input, LSB = false){
     var hexString = input.toString(16);
     if(hexString.length % 2 != 0){
