9fea3b0cb01c1b5aa0d3996e01a67b1cdd7ee9cb
CWKSC <CWKSC.person@gmail.com>
2021-02-07 18:48:11 +0800

    Update include.js

--- MyLib_js/include.js
+++ MyLib_js/include.js
@@ -1,13 +1,18 @@
 (() => {
 
+var root = "";
+
 function load(name){
 	document.write(`<script 
 		type="text/javascript" 
-		src="https://cdn.jsdelivr.net/gh/CWKSC/MyLib_js/MyLib_js/${name}.js"
+		src="https://cdn.jsdelivr.net/gh/CWKSC/MyLib_js/MyLib_js/${root}${name}.js"
 		async> </script>`);
 }
-load("CTFTool");
-load("AdvanceLooping");
+
+["CTFTool", 
+"AdvanceLooping",
+]
+.map(load);
 
 })();
 
