c676d944f01c0f0201bf4e7587d1665211aee70d
CWKSC <CWKSC.person@gmail.com>
2021-02-07 17:52:47 +0800

    Update include.js

--- MyLib_js/include.js
+++ MyLib_js/include.js
@@ -4,7 +4,7 @@ function load(src){
 	document.write(`<script src="${currentRoot}/${src}.js"></script>`);
 }
 function loadByUrl(name){
-	document.write(`<script src="https://raw.githubusercontent.com/CWKSC/MyLib_js/master/MyLib_js/${name}"`)
+	document.write(`<script src="https://raw.githubusercontent.com/CWKSC/MyLib_js/master/MyLib_js/${name}.js"></script>`)
 }
 
 loadByUrl("HelloWorld");
