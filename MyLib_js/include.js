bfe055eda84aa99fe573c2cc9a6a79755b7e26ff
CWKSC <CWKSC.person@gmail.com>
2021-02-07 17:48:13 +0800

    Update include.js

--- MyLib_js/include.js
+++ MyLib_js/include.js
@@ -3,9 +3,15 @@ var currentRoot = "";
 function load(src){
 	document.write(`<script src="${currentRoot}/${src}.js"></script>`);
 }
+function loadByUrl(name){
+	document.write(`<script src="https://raw.githubusercontent.com/CWKSC/MyLib_js/master/MyLib_js/${name}"`)
+}
 
+loadByUrl("HelloWorld");
+loadByUrl("JoinFunction");
+loadByUrl("CTFTool");
 
-currentRoot = "MyLib_js";
-load("HelloWorld");
-load("JoinFunction");
-load("CTFTool");
+// currentRoot = "MyLib_js";
+// load("HelloWorld");
+// load("JoinFunction");
+// load("CTFTool");
---
+++ README.md
@@ -0,0 +1,12 @@
+MyLib_js
+
+```html
+<script src="https://raw.githubusercontent.com/CWKSC/MyLib_js/master/MyLib_js/include.js"></scripts>
+```
+
+`CTFTool`
+
+```html
+<script src="https://raw.githubusercontent.com/CWKSC/MyLib_js/master/MyLib_js/CTFTool.js"></scripts>
+```
+
